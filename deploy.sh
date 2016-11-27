fd9a80e22ac5936c9b79cdc1d8f6766b6a52a163
James Cole <james@xrks.com>
2016-11-27 03:38:10 -0800

    Added cloudflare reset

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 # exit if any command fails
 set -e
 
+# Github repo to push changes to
 DEPLOY_REPO=git@github.com:ch4lox/ch4lox.github.io.git
+# If this is set, also try to purge cloudflare caches
+CLOUDFLARE_ID=89f1dd69a9d7c7b97afeb1b51225dae2
 
 if [ ! -f build/output/index.html ]; then
 	echo 'build/output/index.html does not exist; run build.sh first!'
@@ -15,9 +18,9 @@ if [ ! -z "$DEPLOY_SSH_PRIVATE_KEY" ]; then
 	
 	DEPLOY_SSH_PRIVATE_KEY_FILE=$(mktemp)
 	chmod 600 $DEPLOY_SSH_PRIVATE_KEY_FILE
-	echo "$DEPLOY_SSH_PRIVATE_KEY"|grep -Eo '\-----[^-]+-----'|head -n 1 > $DEPLOY_SSH_PRIVATE_KEY_FILE
-	echo "$DEPLOY_SSH_PRIVATE_KEY"|grep -Eo '[^-]{64,}'|grep -Eo '.{,64}' >> $DEPLOY_SSH_PRIVATE_KEY_FILE
-	echo "$DEPLOY_SSH_PRIVATE_KEY"|grep -Eo '\-----[^-]+-----'|tail -n 1 >> $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo $DEPLOY_SSH_PRIVATE_KEY|grep -Eo '\-----[^-]+-----'|head -n 1 > $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo $DEPLOY_SSH_PRIVATE_KEY|grep -Eo '[^-]{64,}'|grep -Eo '.{,64}' >> $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo $DEPLOY_SSH_PRIVATE_KEY|grep -Eo '\-----[^-]+-----'|tail -n 1 >> $DEPLOY_SSH_PRIVATE_KEY_FILE
 	ssh-add $DEPLOY_SSH_PRIVATE_KEY_FILE
 fi
 
@@ -32,6 +35,20 @@ cd ${TEMP_DIR}
 git add .
 git commit -m "Deployed using deploy.sh by ${USER} on $(hostname -s) at $(date)"
 git push
+
+if [ ! -z "$CLOUDFLARE_ID" ]; then
+	echo "Purging Cloudflare caches..."
+
+	if [ -z "$DEPLOY_CLOUDFLARE_EMAIL" ]; then
+		echo "DEPLOY_CLOUDFLARE_EMAIL environment variable not set";
+	fi
+	if [ -z "$DEPLOY_CLOUDFLARE_KEY" ]; then
+		echo "DEPLOY_CLOUDFLARE_KEY environment variable not set";
+	fi
+	if [ ! -z "$DEPLOY_CLOUDFLARE_KEY" ] && [ ! -z "$DEPLOY_CLOUDFLARE_EMAIL" ]; then
+	curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ID}/purge_cache" -H "X-Auth-Email: ${DEPLOY_CLOUDFLARE_EMAIL}" -H "X-Auth-Key: ${DEPLOY_CLOUDFLARE_KEY}" -H "Content-Type: application/json" --data '{"purge_everything":true}'
+	fi
+fi
 
 cd -
 rm -rf ${TEMP_DIR}
