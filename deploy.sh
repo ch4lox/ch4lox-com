3712564239bba3647ab2954acdee6bccee6269da
James Cole <james@xrks.com>
2016-11-27 02:48:31 -0800

    Fix ssh key file format

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,25 @@
 # exit if any command fails
 set -e
 
+DEPLOY_REPO=git@github.com:ch4lox/ch4lox.github.io.git
+
 if [ ! -f build/output/index.html ]; then
 	echo 'build/output/index.html does not exist; run build.sh first!'
 	exit 1;
 fi
-
-DEPLOY_REPO=git@github.com:ch4lox/ch4lox.github.io.git
-TEMP_DIR=$(mktemp -d)
 
 if [ ! -z "$DEPLOY_SSH_PRIVATE_KEY" ]; then
 	eval "$(ssh-agent -s)"
 	
 	DEPLOY_SSH_PRIVATE_KEY_FILE=$(mktemp)
 	chmod 600 $DEPLOY_SSH_PRIVATE_KEY_FILE
-	echo "$DEPLOY_SSH_PRIVATE_KEY" > $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo $DEPLOY_SSH_PRIVATE_KEY|grep -Eo '\-----[^-]+-----'|head -n 1 > $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo $DEPLOY_SSH_PRIVATE_KEY|grep -Eo '[^-]{64,}'|grep -Eo '.{,64}' >> $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo $DEPLOY_SSH_PRIVATE_KEY|grep -Eo '\-----[^-]+-----'|tail -n 1 >> $DEPLOY_SSH_PRIVATE_KEY_FILE
 	ssh-add $DEPLOY_SSH_PRIVATE_KEY_FILE
 fi
+
+TEMP_DIR=$(mktemp -d)
 
 git clone ${DEPLOY_REPO} ${TEMP_DIR}
 
