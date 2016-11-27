c5fa5d3c66a4b3bb4f7e8807f8e3e8c5366cadb4
James Cole <james@xrks.com>
2016-11-27 02:08:35 -0800

    Trying to setup deploy

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,15 @@ fi
 DEPLOY_REPO=git@github.com:ch4lox/ch4lox.github.io.git
 TEMP_DIR=$(mktemp -d)
 
+if [ ! -z "$DEPLOY_SSH_PRIVATE_KEY" ]; then
+	eval "$(ssh-agent -s)"
+	
+	DEPLOY_SSH_PRIVATE_KEY_FILE=$(mktemp)
+	chmod 600 $DEPLOY_SSH_PRIVATE_KEY_FILE
+	echo "$DEPLOY_SSH_PRIVATE_KEY" > $DEPLOY_SSH_PRIVATE_KEY_FILE
+	ssh-add $DEPLOY_SSH_PRIVATE_KEY_FILE
+fi
+
 git clone ${DEPLOY_REPO} ${TEMP_DIR}
 
 rsync -av --del --exclude='/.git' --exclude='/README.md' --exclude='CNAME' build/output/ ${TEMP_DIR}
